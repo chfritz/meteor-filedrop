004bc07ee5bf727b68eeabd797e8694fc5012463
Christian Fritz <cfritz@parc.com>
2015-10-19 16:28:48 -0700

    Fixed bugs that prevented multiple filedrops to work in the same page.
    - note that this commit requires some reworking of existing code. You
      now need to use the block syntax, i.e.,
      {{#filedrop dropHelper}} body {{#if filedrop_over}}..{{/if}} {{/filedrop}}

--- filedrop.js
+++ filedrop.js
@@ -1,36 +1,43 @@
-// Write your package code here!
 
-var state;
 Template.filedrop.onCreated(function() {
     /* reactive var for state:
        null: no file anywhere in sight
        window: file is being dragged over window
        over: file is held over the drop zone
     */
-    state = new ReactiveVar(null);
+    this.state = new ReactiveVar(null);
 });
 
-Template.registerHelper('filedrop_over', function() {
-    return (state && state.get() == 'over');
-});
-Template.registerHelper('filedrop_window', function() {
-    return (state && state.get() == 'window');
+Template.filedrop.helpers({
+    'all': function() {
+        // We need to manualy hand our desired helpers down to our children.
+        // A usual, we are giving "all" to our children :-) (see template)
+        return _.extend(Template.instance(), {
+            'filedrop_over': function() {
+                return (this.state && this.state.get() == 'over');    
+            },
+            'filedrop_window': function() {
+                return (this.state && this.state.get() == 'window');    
+            },
+            state: Template.instance().state
+        });
+    }
 });
 
 Template.filedrop.onRendered(function() {
 
-    conf = this.data;
-
-    var dropContainer = this.$('>').parent();
+    var conf = this.data;
+    var self = this;
+    var dropContainer = this.$('>');
     var domNode = dropContainer.get(0);
 
     window.addEventListener("dragenter", function(event){
-        state.set('window');
+        self.state.set('window');
         return false;
     }, false);
 
     document.addEventListener("mouseout", function(event){
-        state.set(null);
+        self.state.set(null);
         event.stopPropagation();
         event.preventDefault();
         return false;
@@ -38,20 +45,20 @@ Template.filedrop.onRendered(function() {
 
     domNode.addEventListener("dragenter", function(event){
         conf.onEnter && conf.onEnter(event);
-        state.set('over');
+        self.state.set('over');
     }, false);
 
     domNode.addEventListener("dragover", function(event){
-        // this is important!
+        // self is important!
         event.stopPropagation();
         event.preventDefault();
-        state.set('over');
+        self.state.set('over');
         return false;
     }, false);
 
     domNode.addEventListener("dragexit", function(event){
         conf.onExit && conf.onExit(event);
-        state.set('window');
+        self.state.set('window');
         event.stopPropagation();
         event.preventDefault();
         return false;
@@ -61,7 +68,7 @@ Template.filedrop.onRendered(function() {
         event.stopPropagation();
         event.preventDefault();
         conf.onDrop && conf.onDrop(event.dataTransfer.files);
-        state.set(null);
+        self.state.set(null);
         return false;          
     }, false);
 });
